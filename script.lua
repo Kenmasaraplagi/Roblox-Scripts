8bf888a9b363a77e640c2bc6d256305522837bd6
Kenmasaraplagi <aronkenermitano@gmail.com>
2025-06-02 02:35:53 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,1 @@
--- Sample Roblox Lua Script
--- This script prints a welcome message and runs a basic function
-
-print("✅ Script Loaded from GitHub!")
-
-local function greetPlayer(player)
-    print("Hello, " .. player.Name .. "! Welcome to the game.")
-end
-
--- Connect the function to new players joining
-game.Players.PlayerAdded:Connect(greetPlayer)
+https://paste.ee/r/z1rpMtZR
